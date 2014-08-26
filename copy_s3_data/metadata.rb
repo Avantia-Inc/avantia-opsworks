5135045c7b7849248467c01b3a6695a376a95b3e
Ross Pace <rpace@avantia-inc.com>
2014-08-26 16:23:04 -0400

    moving metadata file after reading https://forums.aws.amazon.com/thread.jspa?messageID=541999#541999

---
+++ copy_s3_data/metadata.rb
@@ -0,0 +1,4 @@
+name			"copy_s3_data"
+description		"copies files from s3 to server"
+license			"MIT"
+version			"0.0.1"
--- metadata.rb
+++
@@ -1,4 +0,0 @@
-name			"avantia-opsworks"
-description		"add external cookbooks to opsworks"
-license			"MIT"
-version			"0.0.1"
